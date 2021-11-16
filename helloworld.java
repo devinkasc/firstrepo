53c1e8ed379f8fba7f7478412040520ab8d07523
devinkasc <94417038+devinkasc@users.noreply.github.com>
2021-11-15 20:39:10 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
   public static void main(String[] args){
-    System.out.println("Welcome to Github from Jenkins")
+    System.out.println("Welcome to Github from Jenkins");
     }
 }
